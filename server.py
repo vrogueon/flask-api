acb46b1c94c43399efc758e56ca3f1a1139891a9
Victor Rodriguez <rodriguezvictor.root@gmail.com>
2021-05-29 17:54:53 -0500

    Adding Bynary search tree

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from sqlalchemy import event
 from flask_sqlalchemy import SQLAlchemy
 import linked_list
 import hash_table
+import binary_search_tree
+import random
 
 # app
 app = Flask(__name__)
@@ -151,9 +153,27 @@ def create_blog_post(user_id):
 
     return jsonify({"message": "Post created"}), 200
 
-@app.route("/user/<user_id>", methods=["GET"])
-def get_all_blog_posts(user_id):
-    pass
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_all_blog_posts(blog_post_id):
+    blog_posts = BlogPost.query.all()
+    random.shuffle(blog_posts)
+
+    bst = binary_search_tree.BinarySearchTree()
+
+    for post in blog_posts:
+        bst.insert({
+            "id": post.id,
+            "title": post.title,
+            "body": post.body,
+            "user_id": post.user_id
+        })
+    
+    post = bst.search(blog_post_id)
+
+    if not post:
+        return jsonify({"message": "post not found"})
+
+    return jsonify(post)
 
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
 def get_one_blog_post(blog_post_id):
